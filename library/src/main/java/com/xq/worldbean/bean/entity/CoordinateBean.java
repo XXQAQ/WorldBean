3ba5a2a2c0dcc09512556133647a964ec99abc41
XQ <120363542@qq.com>
2019-05-08 00:09:37 +0800

    新增一些bean

---
+++ library/src/main/java/com/xq/worldbean/bean/entity/CoordinateBean.java
@@ -0,0 +1,162 @@
+package com.xq.worldbean.bean.entity;
+
+import android.os.Parcel;
+import com.xq.worldbean.bean.behavior.CoordinateBehavior;
+
+public class CoordinateBean extends ParentBean implements CoordinateBehavior {
+
+    protected double x;
+    protected double y;
+    protected double z;
+    protected String coordinateRole;
+
+    public CoordinateBean() {
+    }
+
+    public CoordinateBean(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public CoordinateBean(int id, double x, double y, double z) {
+        super(id);
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "CoordinateBean{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                ", coordinateRole='" + coordinateRole + '\'' +
+                ", tag=" + tag +
+                ", id=" + id +
+                ", idRole='" + idRole + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        CoordinateBean that = (CoordinateBean) o;
+
+        if (Double.compare(that.x, x) != 0) return false;
+        if (Double.compare(that.y, y) != 0) return false;
+        if (Double.compare(that.z, z) != 0) return false;
+        return coordinateRole != null ? coordinateRole.equals(that.coordinateRole) : that.coordinateRole == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        long temp;
+        temp = Double.doubleToLongBits(x);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(z);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (coordinateRole != null ? coordinateRole.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public double getX() {
+        return x;
+    }
+
+    public CoordinateBean setX(double x) {
+        this.x = x;
+        return this;
+    }
+
+    @Override
+    public double getY() {
+        return y;
+    }
+
+    public CoordinateBean setY(double y) {
+        this.y = y;
+        return this;
+    }
+
+    @Override
+    public double getZ() {
+        return z;
+    }
+
+    public CoordinateBean setZ(double z) {
+        this.z = z;
+        return this;
+    }
+
+    @Override
+    public String getCoordinateRole() {
+        return coordinateRole;
+    }
+
+    public CoordinateBean setCoordinateRole(String coordinateRole) {
+        this.coordinateRole = coordinateRole;
+        return this;
+    }
+
+    @Override
+    public CoordinateBean setId(int id) {
+        super.setId(id);
+        return this;
+    }
+
+    @Override
+    public CoordinateBean setIdRole(String idRole) {
+        super.setIdRole(idRole);
+        return this;
+    }
+
+    @Override
+    public CoordinateBean setTag(Object tag) {
+        super.setTag(tag);
+        return this;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeDouble(this.x);
+        dest.writeDouble(this.y);
+        dest.writeDouble(this.z);
+        dest.writeString(this.coordinateRole);
+    }
+
+    protected CoordinateBean(Parcel in) {
+        super(in);
+        this.x = in.readDouble();
+        this.y = in.readDouble();
+        this.z = in.readDouble();
+        this.coordinateRole = in.readString();
+    }
+
+    public static final Creator<CoordinateBean> CREATOR = new Creator<CoordinateBean>() {
+        @Override
+        public CoordinateBean createFromParcel(Parcel source) {
+            return new CoordinateBean(source);
+        }
+
+        @Override
+        public CoordinateBean[] newArray(int size) {
+            return new CoordinateBean[size];
+        }
+    };
+
+}
---
+++ library/src/main/java/com/xq/worldbean/bean/entity/ListBean.java
@@ -0,0 +1,120 @@
+package com.xq.worldbean.bean.entity;
+
+import android.os.Parcel;
+import com.xq.worldbean.bean.behavior.ListBehavior;
+import java.io.Serializable;
+import java.util.List;
+
+public class ListBean<T> extends ParentBean implements ListBehavior<T> {
+
+    protected List<T> list;
+    protected String listRole;
+
+    public ListBean(List<T> list) {
+        this.list = list;
+    }
+
+    public ListBean(int id, List<T> list) {
+        super(id);
+        this.list = list;
+    }
+
+    @Override
+    public String toString() {
+        return "ListBean{" +
+                "list=" + list +
+                ", listRole='" + listRole + '\'' +
+                ", tag=" + tag +
+                ", id=" + id +
+                ", idRole='" + idRole + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        ListBean<?> listBean = (ListBean<?>) o;
+
+        if (list != null ? !list.equals(listBean.list) : listBean.list != null) return false;
+        return listRole != null ? listRole.equals(listBean.listRole) : listBean.listRole == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (list != null ? list.hashCode() : 0);
+        result = 31 * result + (listRole != null ? listRole.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public List<T> getList() {
+        return list;
+    }
+
+    public ListBean<T> setList(List<T> list) {
+        this.list = list;
+        return this;
+    }
+
+    @Override
+    public String getListRole() {
+        return listRole;
+    }
+
+    public ListBean<T> setListRole(String listRole) {
+        this.listRole = listRole;
+        return this;
+    }
+
+    @Override
+    public ListBean setId(int id) {
+        super.setId(id);
+        return this;
+    }
+
+    @Override
+    public ListBean setIdRole(String idRole) {
+        super.setIdRole(idRole);
+        return this;
+    }
+
+    @Override
+    public ListBean setTag(Object tag) {
+        super.setTag(tag);
+        return this;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeSerializable((Serializable) this.list);
+        dest.writeString(this.listRole);
+    }
+
+    protected ListBean(Parcel in) {
+        super(in);
+        this.list = (List<T>) in.readSerializable();
+        this.listRole = in.readString();
+    }
+
+    public static final Creator<ListBean> CREATOR = new Creator<ListBean>() {
+        @Override
+        public ListBean createFromParcel(Parcel source) {
+            return new ListBean(source);
+        }
+
+        @Override
+        public ListBean[] newArray(int size) {
+            return new ListBean[size];
+        }
+    };
+}
