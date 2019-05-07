890876af7882ebff336b3867acc2d2cc0f4918ac
XXQAQ <120363542@qq.com>
2019-05-07 18:01:04 +0800

    兼容优化解析框架

--- library/src/main/java/com/xq/worldbean/bean/behavior/SuccessBehavior.java
+++ library/src/main/java/com/xq/worldbean/bean/behavior/SuccessBehavior.java
@@ -26,6 +26,11 @@ public interface SuccessBehavior extends ParentBehavior{
 
     public boolean isSuccess();
 
+    //getSuccess与isSuccess本质上无任何区别，只是getSuccess针对一些利用反射对方法名解析的框架做了兼容处理
+    default boolean getSuccess(){
+        return isSuccess();
+    }
+
     //上述内容在属性值中扮演的角色，不需要处理判断逻辑可直接返回null
     default String getSuccessRole(){
         return null;
