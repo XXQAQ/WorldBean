238354a0b46145b22abeb14b2269da36b6aedd8f
XXQAQ <120363542@qq.com>
2019-07-24 11:24:15 +0800

    ...

--- library/src/main/java/com/xq/worldbean/util/callback/TCallback.java
+++ library/src/main/java/com/xq/worldbean/util/callback/TCallback.java
@@ -4,6 +4,7 @@ public interface TCallback<T> extends UniverseCallback{
 
     public void onCallback(T t);
 
+    @Deprecated
     @Override
     default void onCallback(Object... objects) {
         if (objects != null && objects.length >= 1) onCallback((T)objects[0]);
